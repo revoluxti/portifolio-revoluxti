05a3b06a4d95b725e27d555208a08d1166efcc4a
Revoluxti <revoluxti@gmail.com>
2026-04-19 01:29:21 -0300

    upgrade
    upgrade

--- Artigo01/revoluxti-platform/vite.config.js
+++ Artigo01/revoluxti-platform/vite.config.js
@@ -12,8 +12,7 @@ export default defineConfig({
     }),
   ],
   // ISSO AQUI RESOLVE A TELA BRANCA EM SUBPASTAS (GITHUB PAGES / LIVE SERVER):
-  base: './', 
-  
+  base: '/portifolio-revoluxti/Artigo01/revoluxti-platform/dist/',  
   build: {
     // Aumenta o limite do aviso para não te incomodar agora (1500kb)
     chunkSizeWarningLimit: 1500,
