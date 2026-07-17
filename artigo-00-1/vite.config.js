645eaa9115c738a62069df601c42160a845edaf7
Revoluxti <revoluxti@gmail.com>
2026-07-17 03:46:57 -0300

    update 17/07/2026 03:46
    update layout nicho 2 de artigos sala do eniac

--- artigo-00-1/vite.config.js
+++ artigo-00-1/vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Se o seu repositório for 'usuario.github.io', deixe './'
-  // Se for 'usuario.github.io/projeto', o ideal é usar o nome do projeto:
-  base: process.env.NODE_ENV === 'production' ? '/nome-do-seu-repositorio/' : '/',
+  base: './', // <--- Isso força o Vite a gerar os caminhos com "./" automaticamente!
 })
