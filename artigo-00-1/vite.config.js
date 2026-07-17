6ad1e91910bfc2861de72d58536bcba327100c0a
Revoluxti <revoluxti@gmail.com>
2026-07-17 04:05:13 -0300

    update layout

--- artigo-00-1/vite.config.js
+++ artigo-00-1/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './', 
   plugins: [react()],
-  base: './', // <--- Isso força o Vite a gerar os caminhos com "./" automaticamente!
 })
